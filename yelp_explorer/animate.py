6786402de5150863a2963d20b9f3907eb43d8a0c
swetharevanur <swetharevanur@Swethas-MacBook-Pro-2.local>
2017-07-28 15:12:55 -0700

    able to generate script for plot that works on HTML page

--- yelp_explorer/animate.py
+++ yelp_explorer/animate.py
@@ -1,11 +1,13 @@
 from bokeh.client import push_session
+# from bokeh.embed import server_document
 from bokeh.embed import autoload_server
 from bokeh.plotting import figure, curdoc
 
-# figure() function auto-adds the figure to curdoc()
 plot = figure()
 plot.circle([1,2], [3,4])
 
-session = push_session(curdoc())
-script = autoload_server(plot, session_id=session.id)
+doc = curdoc()
+doc.add_root(plot)
+
+script = autoload_server(app_path="animate") # note the app path
 print(script)
